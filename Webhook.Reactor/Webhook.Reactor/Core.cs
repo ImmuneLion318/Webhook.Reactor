9d38747ce9bd13dda9f80e7cc9ae104b6e4f7829
Immoon <54381449+ImmuneLion318@users.noreply.github.com>
2021-09-18 16:07:10 -0400

    Add files via upload

---
+++ Webhook.Reactor/Webhook.Reactor/Core.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Webhook.Reactor
+{
+    public class Core
+    {
+        public bool ScanWebhook(string Webhook)
+        {
+            bool Result = false;
+
+            try
+            {
+                using (var Client = new WebClient { Proxy = null })
+                {
+                    string WebhookContent = Client.DownloadString(Webhook);
+                    var JsonData = JsonConvert.DeserializeObject<JToken>(WebhookContent);
+
+                    if (JsonData["type"]?.ToString() == "1")
+                    {
+                        Result = true;
+                    }
+                }
+            }
+            catch
+            {
+                Result = false;
+            }
+
+            return Result;
+        }
+
+        public string RetrieveDetails(string Webhook)
+        {
+            string Data = null;
+
+            if (ScanWebhook(Webhook) == true)
+            {
+                var Client = new WebClient();
+                Client.Proxy = null;
+
+                string WebhookContent = Client.DownloadString(Webhook);
+                var JsonData = JsonConvert.DeserializeObject<JToken>(WebhookContent);
+
+                return string.Concat(new string[]
+                {
+                    $"Type: {JsonData["type"]?.ToString()}\n",
+                    $"ID: {JsonData["id"]?.ToString()}\n",
+                    $"Name: {JsonData["name"]?.ToString()}\n",
+                    $"Avatar: {JsonData["avatar"]?.ToString()}\n",
+                    $"Channel_ID: {JsonData["channel_id"]?.ToString()}\n",
+                    $"Guild_ID: {JsonData["guild_id"]?.ToString()}\n",
+                    $"Application_ID: {JsonData["application_ID"]?.ToString()}\n",
+                    $"Token: {JsonData["token"]?.ToString()}\n",
+                });
+            }
+
+            return Data;
+        }
+
+        public void SendMessage(string Webhook, string Message, string AvatarUrl = null)
+        {
+            HttpClient Client = new HttpClient();
+            Dictionary<string, string> Contents = new Dictionary<string, string>
+                {
+                    { "content", Message },
+                    { "username", "Webhook" },
+                    { "avatar_url", AvatarUrl }
+                };
+            Client.PostAsync(Webhook, new FormUrlEncodedContent(Contents)).GetAwaiter().GetResult();
+        }
+
+        public bool DeleteWebhook(string Webhook)
+        {
+            bool Result = false;
+
+            WebRequest Request = WebRequest.Create(Webhook);
+            Request.Method = "DELETE";
+
+            HttpWebResponse Response = (HttpWebResponse)Request.GetResponse();
+            if (Response.StatusDescription.Contains("No Content"))
+            {
+                Result = true;
+            }
+            return Result;
+        }
+    }
+}
